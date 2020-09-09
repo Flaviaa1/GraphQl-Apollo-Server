5d1653e3d43003390cc83cff6437d715dc9cfb02
Flaviaa1 <flaviagregorio262@gmail.com>
2020-09-09 17:15:12 -0300

    connect MongoDB

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,8 @@
+const bcrypt = require('bcryptjs');
+const User = require('../database/models/user');
 const { tasks, users } = require('../constans/index');
+
+
 module.exports = {
      Query:{
       
@@ -6,7 +10,23 @@ module.exports = {
         user: (_,{id}) =>  users.find(user => user.id === id)      
 
     },
-   
+    Mutation: {
+        signup: async (_, { input }) => {
+          try {
+            const user = await User.findOne({ email: input.email });
+            if (user) {
+              throw new Error('Email already in use');
+            }
+            const hashedPassword = await bcrypt.hash(input.password, 12);
+            const newUser = new User({ ...input, password: hashedPassword });
+            const result = await newUser.save();
+            return result;
+          } catch (error) {
+            console.log(error);
+            throw error;
+          }
+        }
+    },
     User: {
         tasks: ({id}) =>  tasks.filter(task => task.userId === id)
         
--- typeDefs/user.js
+++ typeDefs/user.js
@@ -5,12 +5,23 @@ module.exports = gql`
     users: [User!]
     user (id: ID): User
   }
+  extend type Mutation {
+    signup(input: signupInput): User
+  }
+
+  input signupInput {
+    name: String!
+    email: String!
+    password: String!
+  }
 
   type User {
     id: ID!
     name: String!
     email: String!
     tasks: [Task!]
+    createdAt: Date!
+    updatedAt: Date!
   }
 
 
